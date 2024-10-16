603452c7192a46d61794b301b5d8ba7d07d5c40b
msuzuki90 <muazu.suzuki@gmail.com>
2024-10-16 17:22:01 +0200

    configured and style toggle light/dark mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
+import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ThemeProvider } from './common/ThemeContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )

cd420e5ef020c475563ef5e8d71ff1a19c5049f7
msuzuki90 <muazu.suzuki@gmail.com>
2024-11-02 00:09:39 +0100

    put in place react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
+// App.js
 import './App.css';
-import Hero from './sections/Hero/Hero';
+import NavBar from './sections/NavBar/NavBar';
+import Hero from './sections/Hero/Hero'; // Assuming Hero is your home section
 import Projects from './sections/Projects/Projects';
 import Skills from './sections/Skills/Skills';
 import Contact from './sections/Contact/Contact';
 import Footer from './sections/Footer/Footer';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <Hero />
-      <Projects />
-      <Skills />
-      <Contact />
-      <Footer/>
-    </>
-  )
+    <Router>
+      <NavBar />
+      <Routes>
+        {/* Home Page */}
+        <Route path="/" element={<Hero />} />
+
+        {/* Projects Page */}
+        <Route path="/projects" element={<Projects />} />
+
+        {/* Skills Page */}
+        <Route path="/skills" element={<Skills />} />
+
+        {/* Contact Page */}
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;

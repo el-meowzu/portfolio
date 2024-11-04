a3de5bcddaf447b660aa66336d8698ddd46b8bd6
msuzuki90 <muazu.suzuki@gmail.com>
2024-11-05 00:39:01 +0100

    changed skill layout added animation - svg logo

--- src/common/SkillList.jsx
+++ src/common/SkillList.jsx
@@ -1,10 +1,17 @@
-function SkillList({src, skill}) {
-  return (
-    <span>
-        <img src={src} alt="check Mark" />
-        <p>{skill}</p>
-    </span>
-  );
-}
+import React from 'react';
+import SkillRating from './SkillRating';
+import styles from '../sections/Skills/SkillsStyles.module.css';
 
-export default SkillList
+const SkillList = ({ src, skill,rating }) => {
+  return (
+    <div className={styles.skillItem}>
+      <span className={styles.skillInfo}>
+        <img src={src} alt={`${skill} logo`} />
+        <p>{skill}</p>
+      </span>
+      <SkillRating rating={rating}/>
+    </div>
+  );
+};
+
+export default SkillList;

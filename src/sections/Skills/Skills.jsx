407c690556559803435103f94bfd8f459c345b69
msuzuki90 <muazu.suzuki@gmail.com>
2024-11-05 00:45:34 +0100

    added rating component

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -1,43 +1,62 @@
+import React from 'react';
 import styles from './SkillsStyles.module.css';
-import checkMarkIconDark from '../../assets/checkmark-dark.svg';
-import checkMarkIconLight from '../../assets/checkmark-light.svg';
 import SkillList from '../../common/SkillList';
 import { useTheme } from '../../common/ThemeContext';
+import htmlLogo from '../../assets/skillLogo/html.svg';
+import cssLogo from '../../assets/skillLogo/css.svg';
+import jsLogo from '../../assets/skillLogo/javascript.svg';
+import jQueryLogo from '../../assets/skillLogo/jquery.svg';
+import reactLogo from '../../assets/skillLogo/react.svg';
+import nodeLogo from '../../assets/skillLogo/node.svg';
+import expressLogoLight from '../../assets/skillLogo/express-light.svg';
+import expressLogoDark from '../../assets/skillLogo/express-dark.svg';
+import postreSqlLogo from '../../assets/skillLogo/postgresql.svg';
+import mySqlLogo from '../../assets/skillLogo/mysql.svg';
+import apiLight from '../../assets/skillLogo/api-light.svg';
+import apiDark from '../../assets/skillLogo/api-dark.svg';
+import phpLogo from '../../assets/skillLogo/php.svg';
+import bootstrapLogo from '../../assets/skillLogo/bootstrap.svg';
+import tailwindLogo from '../../assets/skillLogo/tailwind.svg';
+import githubLogo from '../../assets/skillLogo/github.svg';
+import symfonyLight from '../../assets/skillLogo/symfony-light.svg';
+import symfonyDark from '../../assets/skillLogo/symfony-dark.svg';
 
 function Skills() {
 
   const { theme } = useTheme();
-  const checkMarkIcon = theme === 'light' ? checkMarkIconLight : checkMarkIconDark;
+  const expressLogo = theme === 'light' ? expressLogoLight : expressLogoDark;
+  const apiLogo = theme === 'light' ? apiLight : apiDark;
+  const symfonyLogo  = theme === 'light' ? symfonyLight : symfonyDark;
 
   return (
     <section id="skills" className={styles.container}>
       <h2 className='sectionTitle'>Skills</h2>
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="HTML"/>
-        <SkillList src={checkMarkIcon} skill="CSS"/>
-        <SkillList src={checkMarkIcon} skill="JavaScript"/>
-        <SkillList src={checkMarkIcon} skill="jQuery" />
-        <SkillList src={checkMarkIcon} skill="React.js" />
+        <SkillList src={htmlLogo} skill="HTML" rating={4.5}/>
+        <SkillList src={cssLogo} skill="CSS" rating={4.5}/>
+        <SkillList src={jsLogo} skill="JavaScript" rating={4}/>
+        <SkillList src={jQueryLogo} skill="jQuery" rating={4}/>
+        <SkillList src={reactLogo} skill="React.js" rating={3.5} />
       </div>
       <hr />
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="Node.js"/>
-        <SkillList src={checkMarkIcon} skill="Express.js"/>
-        <SkillList src={checkMarkIcon} skill="PostGreSQL"/>
-        <SkillList src={checkMarkIcon} skill="MySQL" />
-        <SkillList src={checkMarkIcon} skill="Restful API" />
+        <SkillList src={nodeLogo} skill="Node.js" rating={3.5}/>
+        <SkillList src={expressLogo} skill="Express.js" rating={3.5}/>
+        <SkillList src={postreSqlLogo} skill="PostGreSQL" rating={3.5}/>
+        <SkillList src={mySqlLogo} skill="MySQL" rating={3.5}/>
+        <SkillList src={apiLogo} skill="Restful API" rating={3}/>
       </div>
       <hr />
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="BootStrap" />
-        <SkillList src={checkMarkIcon} skill="TailwindCss" />
-        <SkillList src={checkMarkIcon} skill="GitHub"/>
-        <SkillList src={checkMarkIcon} skill="PHP" />
-        <SkillList src={checkMarkIcon} skill="Symfony" />
+        <SkillList src={bootstrapLogo} skill="BootStrap" rating={4}/>
+        <SkillList src={tailwindLogo} skill="TailwindCss" rating={3.5}/>
+        <SkillList src={githubLogo} skill="GitHub" rating={4}/>
+        <SkillList src={phpLogo} skill="PHP" rating={3}/>
+        <SkillList src={symfonyLogo} skill="Symfony" rating={3}/>
       </div>
-      <hr />
+  
     </section>
   )
 }
 
-export default Skills
+export default Skills;
